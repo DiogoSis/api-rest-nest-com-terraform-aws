53fa9a7a13cfdce92594c913b3fc31865f95e351
Diogo <diogo@Note-Home.bbrouter>
2024-09-29 13:02:47 -0300

    feat: criação do metodo de identificação de usuario

--- api/src/controller/users.controller.ts
+++ api/src/controller/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
-import { CreateUsersRequestDTO } from './dto/CreateUsersRequest.dto';
-import { CreateUsersResponseDTO } from './dto/CreateUsersResponse.dto';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
+import { CreateUsersRequestDTO } from './dto/create-users-request.dto';
+import { CreateUsersResponseDTO } from './dto/create-users-response.dto';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from '@root/service/users.service';
+import { GetUserByIDResponseDTO } from './dto/get-user-by-id-response.dto';
 
 @ApiTags('users')
 @Controller('users')
@@ -24,5 +25,25 @@ export class UsersController {
       password: createUserBody.password,
     });
     return new CreateUsersResponseDTO(createdUserID);
+  }
+
+  @Get(':userID')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User Found',
+    type: GetUserByIDResponseDTO,
+  })
+  public async getUserByID(
+    @Param('userID') userID: string,
+  ): Promise<GetUserByIDResponseDTO> {
+    const userResult = await this.usersService.getUserByID(userID);
+
+    return new GetUserByIDResponseDTO({
+      id: userResult.id,
+      name: userResult.name,
+      email: userResult.email,
+      createdAt: userResult.createdAt.toISOString(),
+      updatedAt: userResult.updatedAt.toISOString(),
+    });
   }
 }
